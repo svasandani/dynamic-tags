fec61702072dc5d7700678ee7a97cdf99abee9d9
svasandani <vasandanishailesh@gmail.com>
2020-07-04 23:09:20 -0400

    Update params to be more opinonated

--- js/dynamic-tags.js
+++ js/dynamic-tags.js
@@ -1,39 +1,62 @@
 class DynamicTagController {
+  /*  PARAMS
+
+      -- selectors --
+      container: CSS selector for the container, defaults to "container"
+      filter: CSS selector for the filter, defaults to "filter"
+      card: class selector for each card to be filtered, defaults to "card"
+      tag: class selector for each tag, defaults to "tag"
+
+      -- config --
+      useDataset: set to any value to use "data-X" attribute instead of innerHTML, defaults to false
+      dataTag: defines the "X" in the "data-X" attribute, defaults to "tag" as in "data-tag"
+      filterTagType: defines what type of element to add to filter, defaults to "span"
+      activeTagClass: defines what class to assign to active tags, defaults to "active"
+
+      -- no result behavior --
+      noResultError: defined whether to show an error when no result, defaults to true
+      noResultType: defines what type of element to use as the no result error, defaults to "h4"
+      noResultText: defines custom text for no result error, defaults to "Sorry, nothing matches your filters."
+
+  */
+
   constructor(params) {
-    this.containerSelector = params["container"];
-    this.elementSelector = params["element"];
-    this.tagSelector = params["tag"];
-    this.filterSelector = params["filter"];
+    this.containerClass = params["container"] != null ? params["container"] : "container";
+    this.filterClass = params["filter"] != null ? params["filter"] : "filter";
+    this.cardClass = params["card"] != null ? params["card"] : "card";
+    this.tagClass = params["tag"] != null ? params["tag"] : "tag";
 
-    this.useDataset = params["useDataset"] == true;
-    this.dataTag = params["dataTag"];
-    this.updateMethod = params["method"];
+    this.useDataset = params["useDataset"] != null;
+    this.dataTag = params["dataTag"] != null ? params["dataTag"] : "tag";
+    this.filterTagType = params["filterTagType"] != null ? params["filterTagType"] : "span";
+    this.activeTagClass = params["activeTagClass"] != null ? params["activeTagClass"] : "active";
 
-    this.noResult = document.createElement("h4");
-    this.noResult.appendChild(params["noResult"] != null ? document.createTextNode(params["noResult"]) : document.createTextNode("Sorry, nothing matches your filters."));
+    this.noResultError = params["noResultError"] == "false" ? false : true;
+    this.noResult = this.noResultError ? document.createElement(params["noResultType"] != null ? params["noResultType"] : "h4") : "";
+    if (this.noResultError) this.noResult.appendChild(params["noResultText"] != null ? document.createTextNode(params["noResultText"]) : document.createTextNode("Sorry, nothing matches your filters."));
 
-    this.container = document.querySelector(this.containerSelector);
-    this.allElements = document.querySelectorAll(this.elementSelector) || [];
-    this.activeElements = this.allElements;
+    this.filter = document.querySelector("." + this.filterClass);
+    this.container = document.querySelector("." + this.containerClass);
+    this.allCards = this.container.querySelectorAll("." + this.cardClass) || [];
+    this.activeCards = this.allCards;
     this.tagDict = {};
     this.tagFilter = [];
-    this.filter = document.querySelector(this.filterSelector);
   }
 
   getTag(el) {
-    if (this.useDataset) return el.getAttribute("data-" + dataTag);
+    if (this.useDataset) return el.getAttribute("data-" + this.dataTag);
     else return el.innerHTML;
   }
 
   refreshElements() {
-    this.allElements = document.querySelectorAll(this.elementSelector) || [];
+    this.allCards = this.container.querySelectorAll("." + this.cardClass) || [];
   }
 
   loadTags() {
     this.refreshElements();
 
-    this.allElements.forEach(el => {
-      el.querySelectorAll(this.tagSelector).forEach(tag => {
+    this.allCards.forEach(el => {
+      el.querySelectorAll("." + this.tagClass).forEach(tag => {
         let dataTag = this.getTag(tag);
         if (typeof this.tagDict[dataTag] == "undefined") {
           this.tagDict[dataTag] = [el];
@@ -44,15 +67,20 @@ class DynamicTagController {
     });
 
     for (let tag in this.tagDict) {
-      let el = document.createElement("span");
+      let el = document.createElement(this.filterTagType);
       el.appendChild(document.createTextNode(tag));
-      el.classList.add(this.tagSelector.split(".")[1]);
-      if (this.useDataset) el.setAttribute("data-" + dataTag, tag);
+      el.classList.add(this.tagClass);
+      if (this.useDataset) el.setAttribute("data-" + this.dataTag, tag);
       this.filter.append(el);
     }
 
+    this.filter.querySelectorAll("." + this.tagClass).forEach(tag => {
+      tag.addEventListener('click', () => {
+        this.toggleTag(this.getTag(tag));
+      }, false);
+    });
 
-    document.querySelectorAll(this.tagSelector).forEach(tag => {
+    this.container.querySelectorAll("." + this.tagClass).forEach(tag => {
       tag.addEventListener('click', () => {
         this.toggleTag(this.getTag(tag));
       }, false);
@@ -60,9 +88,9 @@ class DynamicTagController {
   }
 
   filterElements() {
-    this.activeElements = Array.from(this.allElements);
+    this.activeCards = Array.from(this.allCards);
     this.tagFilter.forEach(tag => {
-      this.activeElements = this.activeElements.filter(el => this.tagDict[tag].includes(el));
+      this.activeCards = this.activeCards.filter(el => this.tagDict[tag].includes(el));
     });
   }
 
@@ -71,21 +99,21 @@ class DynamicTagController {
       this.container.removeChild(this.container.lastChild);
     }
 
-    if (this.activeElements.length == 0) {
+    if (this.activeCards.length == 0) {
       this.container.append(this.noResult);
     } else {
-      this.activeElements.forEach(el => {
+      this.activeCards.forEach(el => {
         this.container.append(el);
       });
     }
   }
 
   updateTagActive() {
-    document.querySelectorAll(this.tagSelector).forEach(tag => {
+    document.querySelectorAll("." + this.tagClass).forEach(tag => {
       if (this.tagFilter.includes(this.getTag(tag))) {
-        tag.classList.add("active");
+        tag.classList.add(this.activeTagClass);
       } else {
-        tag.classList.remove("active");
+        tag.classList.remove(this.activeTagClass);
       }
     });
   }
@@ -99,7 +127,7 @@ class DynamicTagController {
     }
 
     if (this.tagFilter.length == 0) {
-      this.activeElements = this.allElements;
+      this.activeCards = this.allCards;
       this.updateElements();
       this.updateTagActive();
       return;
--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var dtc = new DynamicTagController({
-  "container": ".container",
-  "element": ".el",
-  "tag": ".tag",
-  "filter": ".filter",
-  "method": "sliding"
+  "container": "container",
+  "card": "el",
+  "tag": "tag",
+  "filter": "filter",
+  // "useDataset": "true",
+  "dataTag": "tag"
 })
 
 dtc.loadTags();
 
 var dtc2 = new DynamicTagController({
-  "container": ".container2",
-  "element": ".el2",
-  "tag": ".tag2",
-  "filter": ".filter2",
+  "container": "container2",
+  "card": "el2",
+  "tag": "tag2",
+  "filter": "filter2",
   "noResult": "Ain't nuthin' here"
 })
 
